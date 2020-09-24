7370e9a8815720a60a86d3ad3b52071b4c416097
Ruben de Bruijn <ruben.de.bruijn@carcollect.com>
2020-09-24 21:03:51 +0200

    basics server

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongodb = require('./config/mongo.db');
+const env = require('./config/env');
+
+const transactions_routes = require('./api/transactions.routes');
+
+// initialize express
+const app = express();
+module.exports = {};
+
+// CORS headers
+app.use(cors());
+
+// bodyParser
+app.use(bodyParser.json());
+
+//Endpoints
+app.get('/', (res: { send: (arg0: string) => void; }) => {
+    res.send('Hello BlockChain')
+  });
+app.use('/api/v1', transactions_routes);
+
+// Run server
+app.listen(env.env.webPort, function () {
+  console.log('Served on port ' + env.env.webPort)
+});
+
+module.exports = app;
